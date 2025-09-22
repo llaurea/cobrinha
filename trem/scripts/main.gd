82b14f0a24d4fbcbea18b9dc95f79d80a0f7c73b
llaurea <laurasguimaraes.2017@gmail.com>
2025-09-22 10:52:14 -0300

    jogo
    jogo cobrinha todo funcionando, só falta a tela de menu

---
+++ trem/scripts/main.gd
@@ -0,0 +1,140 @@
+extends Node
+
+@export var cobra_scene : PackedScene
+
+#variaveis do jogo
+var score : int
+var game_started : bool = false
+
+#variaveis do grid
+var cells : int = 20
+var cell_size : int = 50
+
+#variaveis comida
+var food_pos : Vector2
+var regen_food : bool = true
+
+#variaveis cobra
+var old_data : Array
+var snake_data : Array
+var snake : Array
+
+#variaveis de movimento
+var start_pos = Vector2(9, 9)
+var up = Vector2(0, -1)
+var down = Vector2(0, 1)
+var left = Vector2(-1, 0)
+var right = Vector2(1, 0)
+var move_direction : Vector2
+var can_move : bool
+
+func _ready():
+	new_game()
+
+func new_game():
+	get_tree().paused = false
+	get_tree().call_group("segments", "queue_free")
+	$gameovermenu.hide()
+	score = 0
+	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score)
+	move_direction = up
+	can_move = true
+	generate_snake()
+	move_food()
+
+func generate_snake():
+	old_data.clear()
+	snake_data.clear()
+	snake.clear()
+	#começa com o "start_pos", aumenta o rabo p baixo verticalmente
+	for i in range(3):
+		add_segment(start_pos + Vector2(0, i))
+
+func add_segment(pos):
+	snake_data.append(pos)
+	var cobra = cobra_scene.instantiate()
+	cobra.position = (pos * cell_size) + Vector2(0, cell_size)
+	add_child(cobra)
+	snake.append(cobra)
+
+func _process(delta):
+	move_snake()
+
+func move_snake():
+	if can_move:
+		#update no movimento toda vez q apertar uma tecla
+		if Input.is_action_just_pressed("move_down") and move_direction != up:
+			move_direction = down
+			can_move = false
+			if not game_started:
+				start_game()
+		if Input.is_action_just_pressed("move_up") and move_direction != down:
+			move_direction = up
+			can_move = false
+			if not game_started:
+				start_game()
+		if Input.is_action_just_pressed("move_left") and move_direction != right:
+			move_direction = left
+			can_move = false
+			if not game_started:
+				start_game()
+		if Input.is_action_just_pressed("move_right") and move_direction != left:
+			move_direction = right
+			can_move = false
+			if not game_started:
+				start_game()
+
+func start_game():
+	game_started = true
+	$MoveTimer.start()
+
+func _on_move_timer_timeout():
+	#permite movimentacao da croba
+	can_move = true
+	#usar a opsicao antiga da cobra pra mover os pedaços
+	old_data = [] + snake_data
+	snake_data[0] += move_direction
+	for i in range(len(snake_data)):
+		#mexer os pedaçõs um pra frente
+		if i > 0:
+			snake_data[i] = old_data[i - 1]
+		snake[i].position = snake_data[i] * cell_size + Vector2(0, cell_size)
+	check_out_of_bounds()
+	check_self_eaten()
+	check_food_eaten()
+
+func check_out_of_bounds():
+	if snake_data[0].x < 0 or snake_data[0].x > cells - 1 or snake_data[0].y < 0 or snake_data[0].y > cells - 1:
+		end_game()
+
+func check_self_eaten():
+	for i in range(1, len(snake_data)):
+		if snake_data[0] == snake_data[i]:
+			end_game()
+
+func move_food():
+	while regen_food:
+		regen_food = false
+		food_pos = Vector2(randi_range(0, cells - 1), randi_range(0, cells - 1))
+		for i in snake_data:
+			if food_pos == i:
+				regen_food = true
+	$food.position = (food_pos * cell_size)+ Vector2(0, cell_size)
+	regen_food = true
+
+func check_food_eaten():
+	#se a cobra comer, cresce
+	if snake_data[0] == food_pos:
+		score += 1
+		$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score)
+		add_segment(old_data[-1])
+		move_food()
+
+func end_game():
+	$gameovermenu.show()
+	$MoveTimer.stop()
+	game_started = false
+	get_tree().paused = true
+
+func _on_gameovermenu_restart():
+	new_game()
